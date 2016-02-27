b63bcc97a490770daaeeb85eb06eda4b7fb2f5bf
ronprotempus <sales@protempus.com>
2016-02-27 18:37:03 -0500

    Before adding angular

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+     var  headerHeight = 0;
+     headerHeight = $('header').height() + 80;
+     
+    $('#navf01').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f1').offset().top - headerHeight }, 'slow');
+    });
+     $('#navf02').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f2').offset().top -  headerHeight }, 'slow');
+    });    
+     $('#navf03').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f3').offset().top -  headerHeight }, 'slow');
+    });   
+     
+    $('#navf04').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f4').offset().top -  headerHeight }, 'slow');
+    });
+     $('#navf05').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f5').offset().top -  headerHeight }, 'slow');
+    });    
+     $('#navf06').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f6').offset().top -  headerHeight }, 'slow');
+    }); 
+     
+    $('#navf07').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f7').offset().top -  headerHeight }, 'slow');
+    });
+     $('#navf08').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f8').offset().top -  headerHeight }, 'slow');
+    });    
+     $('#navf09').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f9').offset().top -  headerHeight }, 'slow');
+    });   
+     
+     $('#navf10').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f10').offset().top -  headerHeight }, 'slow');
+    });  
+	
+    $('#navf11').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f11').offset().top -  headerHeight }, 'slow');
+    });  	
+	
+    $('#navf12').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f12').offset().top -  headerHeight }, 'slow');
+    });  	
+     
+	    $('#navf13').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f13').offset().top -  headerHeight }, 'slow');
+    });  	
+     
+	    $('#navf14').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f14').offset().top -  headerHeight }, 'slow');
+    });  	
+     
+	    $('#navf15').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f15').offset().top -  headerHeight }, 'slow');
+    });  	
+     
+	    $('#navf16').click(function(e) {  
+      $('html, body').animate({scrollTop: $('#f16').offset().top -  headerHeight }, 'slow');
+    });  	
+     
+     
+});   
