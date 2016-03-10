e7a684342c951a698e9bd4c63fe4d790f739eb0b
ronprotempus <sales@protempus.com>
2016-03-10 18:07:05 -0500

    Angular additions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,104 @@
-var app = angular.module('app', []);
-app.controller('ctrl1', function($scope,$http,$log) {
+(function() {
+  var app = angular.module('myApp', [])
+    .controller('featureController', function ($scope,$log,$anchorScroll, $location) {
+        $scope.features = featureList;
 
-});
+	$scope.mouseEvent = function(feature, bool) {
+    if(bool === true) {
+		this.over = true;
+        this.cols = -300;
+		this.featureClass = "hovering";
+		this.scrollTo(this.feature.nav);
+    } else if (bool === false) {
+		this.over = false;
+	    this.featureClass = "notHovering";		
+        this.cols = 100;
+    }
+	};
+		
+	// Scroll to the top of the section for this feature
+	// Section ID is json nav item below linked to section ID
+     $scope.scrollTo = function(featureLoc) {
+		
+		  $location.hash( this.feature.nav);
+		  $anchorScroll.yOffset = 180; // add offset for header
+		  $anchorScroll();
+           
+      };		
+		
+});		
+	
+
+
+	
+var featureList = [{
+    "item": "SECURE - non cloud based software_____",
+	"nav":"f1"
+}, 
+{
+    "item": "Easy time keeping______________________",
+	"nav":"f2"
+}, 
+{
+    "item": "Quickly bill your clients______________",
+	"nav":"f3"
+	
+}, 
+{
+    "item": "Complete AR______________________________",
+	"nav":"f4"
+}, 
+{
+    "item": "Trust Accounting_________________________",
+	"nav":"f5"
+}, 
+{
+    "item": "Contact Management______________________",
+	"nav":"f6"
+}, 
+{
+    "item": "Case Management_________________________",
+	"nav":"f7"
+}, 
+{
+    "item": "Document Management_______________________",
+	"nav":"f8"
+},
+{
+    "item": "Task Management_______________________",
+	"nav":"f9"
+}, 				   
+{
+    "item": "Quickbooks interface__________________",
+	"nav":"f10"
+},
+{
+    "item": "Google Sync___________________________",
+	"nav":"f11"
+},
+{
+    "item": "Outlook sync___________________________",
+	"nav":"f12"
+},
+{
+    "item": "Secure / Limited Remote Access_________",
+	"nav":"f13"
+},
+{
+    "item": "Free Support____________________________",
+	"nav":"f14"
+},	
+{
+    "item": "Free Training___________________________",
+	"nav":"f15"
+},
+		   
+	
+{
+    "item": "Free data conversion____________________",
+	"nav":"f16"
+}];
+	
+		
+    
+})();
--- js/main.js
+++ js/main.js
@@ -1,64 +1,19 @@
 $(document).ready(function() {
-     var  headerHeight = 0;
-     headerHeight = $('header').height() + 80;
      
-    $('#navf01').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f1').offset().top - headerHeight }, 'slow');
-    });
-     $('#navf02').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f2').offset().top -  headerHeight }, 'slow');
-    });    
-     $('#navf03').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f3').offset().top -  headerHeight }, 'slow');
-    });   
-     
-    $('#navf04').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f4').offset().top -  headerHeight }, 'slow');
-    });
-     $('#navf05').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f5').offset().top -  headerHeight }, 'slow');
-    });    
-     $('#navf06').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f6').offset().top -  headerHeight }, 'slow');
-    }); 
-     
-    $('#navf07').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f7').offset().top -  headerHeight }, 'slow');
-    });
-     $('#navf08').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f8').offset().top -  headerHeight }, 'slow');
-    });    
-     $('#navf09').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f9').offset().top -  headerHeight }, 'slow');
-    });   
-     
-     $('#navf10').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f10').offset().top -  headerHeight }, 'slow');
-    });  
+    
 	
-    $('#navf11').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f11').offset().top -  headerHeight }, 'slow');
-    });  	
-	
-    $('#navf12').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f12').offset().top -  headerHeight }, 'slow');
-    });  	
-     
-	    $('#navf13').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f13').offset().top -  headerHeight }, 'slow');
-    });  	
-     
-	    $('#navf14').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f14').offset().top -  headerHeight }, 'slow');
-    });  	
-     
-	    $('#navf15').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f15').offset().top -  headerHeight }, 'slow');
-    });  	
-     
-	    $('#navf16').click(function(e) {  
-      $('html, body').animate({scrollTop: $('#f16').offset().top -  headerHeight }, 'slow');
-    });  	
-     
-     
+    // the AJAX part
+    var quoteAPI = "http://quotesondesign.com/api/3.0/api-3.0.json";
+    
+    function displayPhotos(data) {
+      var photoHTML = '<ul>';
+      $.each(data.items,function(i,photo) {
+        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        photoHTML += '<a href="' + photo.link + '" class="image">';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      }); // end each
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);
+    }
+    $.getJSON(flickerAPI, flickrOptions, getQuote);	
 });   
