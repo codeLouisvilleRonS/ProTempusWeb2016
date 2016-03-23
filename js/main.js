14d14e7f27947741b99f0ae0c9f759e17b8c6734
ronprotempus <sales@protempus.com>
2016-03-23 17:03:22 -0400

    Version .95

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
      
+	// Get Quote of the day
     $.ajax({type: "POST",
             crossDomain : true,
             url: "http://api.forismatic.com/api/1.0/",
@@ -10,11 +11,13 @@ $(document).ready(function() {
     });
 });
 
+/******  Add Quote of the day to page ********/
 
-function myJsonMethod(response){	
+function myJsonMethod(response){
+	
  	//alert(JSON.stringify(response));	
 
-  $('#log').prepend('<pre>' + $('#response').html() + '</pre>');
+  $('#log').prepend('<div>' + $('#response').html() + '</div>');
   $('#response').html(response.quoteText + " - " + response.quoteAuthor);
 }
 
